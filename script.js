92a15119a43060fe84d1c94ea2c9475502f3b67e
Jacob Dominic <>
2021-05-11 12:46:33 +0000

    changes 1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('area').addEventListener('click', areaButton)
 document.getElementById('circumference').addEventListener('click', circumferenceButton)
-//Connects buttons to area and circumference
+// Connects buttons to area and circumference
 
 let diameter = 0
 let area = 0
 let circumference = 0
 let pinumber = Math.PI
-//Sets Variables/Let statements
+// Sets Variables/Let statements
 
 function areaButton () {
   diameter = document.getElementById('number').value
@@ -14,8 +14,8 @@ function areaButton () {
   pinumber = parseFloat(pinumber)
 
   area = pinumber * (diameter / 2) ** 2
-  document.getElementById('Area-Answer').innerHTML = area
-} 
+  document.getElementById('area-answer').innerHTML = area
+}
 // Function of Area Button
 
 function circumferenceButton () {
@@ -24,6 +24,6 @@ function circumferenceButton () {
   pinumber = parseFloat(pinumber)
 
   circumference = pinumber * diameter
-  document.getElementById('Circumference-Answer').innerHTML = circumference
+  document.getElementById('circumference-answer').innerHTML = circumference
 }
 // Function of Circumference Button
